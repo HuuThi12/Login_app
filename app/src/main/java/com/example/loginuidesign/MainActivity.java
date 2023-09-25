80105650348d223bdaae770961ba92088f804d1c
HuuThi12 <huuthi12072003@gmail.com>
2023-09-25 22:24:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginuidesign/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.loginuidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demo_constrain.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
